4902a520031f520b7c5c83f09596e496185d8292
Mohammad Fowzan <126281616+mfowzan@users.noreply.github.com>
2023-08-16 23:36:14 +0530

    Add files via upload
    Updated code

---
+++ phonebookmanager.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <conio.h>
+
+struct contact
+{
+    char name[35];
+    char ph_number[10];
+    //char address[35]
+    //char email[35]
+    
+};
+void addcontact();
+void editcontact();
+void search();
+
+struct contact phonebook[100];
+int serial_no=0;
+
+
+void start()
+{
+    
+    
+    printf("Menu:\n");
+    printf("1.Add    \t2.Edit  \t3.Delete   \n\n4.List      5.Search\n\n");
+    char choice;
+    scanf(" %c%*c", &choice);
+    
+
+    switch(choice)
+    {
+        case '1':
+           addcontact();
+           break;
+        case '2':
+           editcontact();
+           break;
+        //case '3':
+           //deletecontact();
+           //break;
+        case '4':
+           list();
+           break;
+        case '5':
+           search();
+           break;
+        default:
+           printf("Invalid choice. Please select valid option\n");
+           start();
+           
+    }    
+    
+    
+           
+}   
+
+void addcontact()
+    {
+    
+        printf("Enter Name:\n");
+        scanf("%s",phonebook[serial_no].name);
+        printf("Enter mobile number\n");
+        scanf("%s",phonebook[serial_no].ph_number);
+        printf("<Contact added successfully>\n\n");
+        serial_no++;
+        printf("enter any key")
+        getch()
+        start();
+
+    }
+
+void editcontact()
+    {
+        int i;
+        int temp=0;
+        char con[35];
+        char namee[35];
+        char phone[10];
+        printf("write contact name you want to edit");
+        scanf("%s",con);
+        
+        for (i=0;i<serial_no;i++)
+        {
+            if (strcmp(phonebook[i].name,con)==0)
+            {
+                printf("enter new name\n");
+                scanf("%s",phonebook[i].name);
+                printf("enter new mobile number\n");
+                scanf("%s",phonebook[i].ph_number);
+                printf("contact edited successfully");
+                temp=1;
+                break;
+        
+            }
+        }
+            
+        if (temp==0)
+        {
+          printf("contact not found");
+        }
+        printf("enter any key")
+        getch()
+        start();
+    
+    }
+    
+void search()
+{
+    char con[35];
+    int i;
+    int temp=0;
+    printf("enter the contact name you want to search");
+    scanf("%s",con);
+  
+    for (i=0;i<=serial_no;i++)
+    {
+        if (strcmp(phonebook[i].name,con)==0)
+        {
+            printf("Contact found:\n");
+            printf("Name: %s\n",phonebook[i].name);
+            printf("Phone number: %s\n",phonebook[i].ph_number);
+            temp=1;
+            break;
+        }
+        if (temp==0)
+        {
+            printf("contact not found");
+        }
+        
+        
+        
+    }
+    printf("enter any key")
+    getch()  
+    start();
+}
+
+void list()
+{
+    int i=0;
+    while (i<serial_no);
+    {
+        printf("The list of contacts is %s\n",phonebook[i].name);
+        i++;
+    }
+printf("enter any key")
+getch();
+start();
+}
+
+void delete()
+{
+    int i;
+    int temp=0;
+    char con[35];
+    char namee[35];
+    //char phone[10];
+    printf("write contact name you want to edit");
+    scanf("%s",con);
+        
+    for (i=0;i<serial_no;i++)
+    {
+        if (strcmp(phonebook[i].name,con)==0)
+            {
+            phonebook[i].name='\0'
+            phonebook[i].ph_number='\0'
+            
+            printf("contact deleted successfully");
+            temp=1;
+            break;
+        
+            }
+    }
+    if (temp==0)
+    {
+        printf("contact not found in record");
+    }
+    printf("enter any key");
+    getch();
+    start();
+
+}
+
+
+
+
+
+
+int main() 
+{
+printf("****** welcome to phonebook*****\n");
+start();
+
+    
+}
